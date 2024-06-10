a8517d0ae9aa53aa1ecbff4891fe59d4800b48b9
nivindulakshitha <nivindulakshitha@gamil.com>
2024-06-10 22:21:13 +0530

    Update network configuration in app.js

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 const { exec } = require('child_process');
 
-const ssid = 'MyAdHocNetwork'; // Name of the network
-const password = 'MyPassword'; // Password of the network
+const ssid = 'ssid'; // Name of the network
+const password = 'password'; // Password of the network
 
-const command = `powershell -Command "Start-Process cmd -ArgumentList '/c netsh wlan set hostednetwork mode=allow ssid=${ssid} key=${password} & netsh wlan start hostednetwork' -Verb runAs"`;
+const command = `powershell -Command "Start-Process cmd -ArgumentList '/c netsh wlan set hostednetwork mode=allow ssid=\\"${ssid}\\" key=${password} & netsh wlan start hostednetwork' -Verb runAs"`;
 
 exec(command, (error, stdout, stderr) => {
     if (error) {
