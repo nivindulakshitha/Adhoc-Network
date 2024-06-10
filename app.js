90ed4da7a36c323633237d4c9815d7cc7a96b4c3
nivindulakshitha <nivindulakshitha@gamil.com>
2024-06-10 21:22:13 +0530

    Initial commit

---
+++ app.js
@@ -0,0 +1,18 @@
+const { exec } = require('child_process');
+
+const ssid = 'MyAdHocNetwork';
+const password = 'MyPassword';
+
+const command = `powershell -Command "Start-Process cmd -ArgumentList '/c netsh wlan set hostednetwork mode=allow ssid=${ssid} key=${password} & netsh wlan start hostednetwork' -Verb runAs"`;
+
+exec(command, (error, stdout, stderr) => {
+    if (error) {
+        console.error(`Error executing command: ${command}\n${error.message}`);
+        return;
+    }
+    if (stderr) {
+        console.error(`stderr: ${stderr}`);
+        return;
+    }
+    console.log(`stdout: ${stdout}`);
+});
